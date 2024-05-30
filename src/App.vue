d4ea4aab6606672284abec4fac6ed32a38f24b60
antoniofilippa94@gmail.com <antoniofilippa94@gmail.com>
2024-05-30 15:31:30 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+  }
+}
+</script>
+<template>
+ 
+ <app-header></app-header>
+ <app-main></app-main>
+
+</template>
+
+<style lang="scss" scoped>
+@use 'src/scss/main.scss'
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+  data() {
+    return {
+        links: [
+            {
+                text: "Donna",
+                url : "https://www.google.com",
+                
+            },
+            {
+                text: "Uomo",
+                url : "https://www.google.com",
+               
+            },
+            {
+                text: "Bambini",
+                url : "https://www.google.com",
+               
+            },
+          
+        ],
+       
+    };
+    
+    }
+}
+
+</script>
+<template>
+    <header>
+        <div class="header-container">
+
+
+            <div class="header-links">
+
+                 <ul>
+                    <a href=""><li v-for="link in links" v-bind:key="link.text">{{ link.text }}</li></a>
+                </ul>
+            </div>
+        
+               
+          
+            <div class="header-logo">
+
+                <img src="../assets/img/boolean-logo.png" alt="logo">
+            </div>
+            <div class="header-icons">
+                <i class="fa-regular fa-user"></i>
+                <i class="fa-regular fa-heart"></i>
+                <i class="fa-solid fa-bag-shopping"></i>
+            </div>
+
+
+
+        </div>
+
+    </header>
+
+</template>
+
+<style scoped>
+
+</style>
+
